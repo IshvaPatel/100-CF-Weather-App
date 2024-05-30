8dffb0b2dac3b8f391d297be93babb56c6e7b428
IshvaPatel <140202582+IshvaPatel@users.noreply.github.com>
2024-05-30 23:44:30 +0530

    Update weather.js

--- static/weather.js
+++ static/weather.js
@@ -5,14 +5,20 @@ document.addEventListener('DOMContentLoaded', () => {
 async function fetchWeather() {
     console.log("Hello")
     const city = document.getElementById('city').value;
+    if (!city) {
+        alert('Please enter a city name.');
+        return;
+    }
     const weatherInfoElement = document.getElementById('weather-info');
     const weatherInfoElement2 = document.getElementById('weather-info2');
     const temperatureElement = document.getElementById('temperature');
     const humidityElement = document.getElementById('humidity');
     const windSpeedElement = document.getElementById('wind-speed');
-    weatherInfoElement.style.display = 'block';
-    weatherInfoElement2.style.display = 'block';
-    weatherInfoElement.innerText = 'Loading...';
+    const loaderElement = document.getElementById('loader2');
+    
+    weatherInfoElement.style.display = 'none';
+    weatherInfoElement2.style.display = 'none';
+    loaderElement.style.display = 'block';
 
     const weatherApiKey1 = '7dfcf22c94fa8abff845e481a769bef4'; // Your OpenWeatherMap API key
     const weatherApiKey = '105c37a85acf04c0265486c669d39389';
@@ -27,7 +33,7 @@ async function fetchWeather() {
         if (!geoencodingData.length) {
             throw new Error('Geocoding error: No results found');
         }
-        const { lat, lon } = geoencodingData[0] ;
+        const { lat, lon } = geoencodingData[0];
 
         const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
         const weatherResponse = await fetch(weatherUrl);
@@ -35,26 +41,32 @@ async function fetchWeather() {
             throw new Error('Weather request failed: ' + weatherResponse.statusText);
         }
         const weatherData = await weatherResponse.json();
-        const currentWeather = weatherData.current;
         const temperature = weatherData.main.temp;
         const humidity = weatherData.main.humidity;
         const windSpeed = weatherData.wind.speed;
         const weatherDescription = weatherData.weather[0].description;
-        const weatherIcon = weatherData.weather[0].icon;
         temperatureElement.innerText = `${temperature} K`;
         humidityElement.innerText = `${humidity} %`;
         windSpeedElement.innerText = `${windSpeed} m/s`;
+        weatherInfoElement.style.display = 'block';
         weatherInfoElement2.style.display = 'block';
         weatherInfoElement.innerText = `Description: ${weatherDescription}`;
     } catch (error) {
         weatherInfoElement.innerText = `Error: ${error.message}`;
         weatherInfoElement.style.display = 'block'; // Show the error message
+    } finally {
+        loaderElement.style.display = 'none';
     }
+}
+
+function showLoader() {
+    const loaderElement = document.getElementById('loader');
+    loaderElement.style.display = 'block';
 }
 
 function saveFavorite() {
     const city = document.getElementById('city').value;
-    
+
     if (!city) {
         alert('Please enter a city name to save as favorite.');
         return;
