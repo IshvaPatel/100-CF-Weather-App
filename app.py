1c13a13d1da207629b0e45dbadc64465555c98a6
IshvaPatel <140202582+IshvaPatel@users.noreply.github.com>
2024-05-30 23:11:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,20 +10,18 @@ def forecaster():
         city = request.form.get('city')
         if city:
             forecasting = Forecasting(city)
+            ##print("Hello")
             forecasting.getHistoricalHourlyData()
             forecasting.LSTM()
             forecast_df = forecasting.generate_forecast(forecast_period=10)
 
-            # Convert index to string for template
             forecast_df.index = forecast_df.index.strftime('%Y-%m-%d %H:%M:%S')
             forecast_data = forecast_df.reset_index().to_dict(orient='records')
-
             return render_template('index.html', city=city, forecast_data=forecast_data)
         else:
             return "City not provided", 400
     else:
-        # Handle GET request, maybe redirect to home page
-        return render_template('index.html')
+        return render_template('index.html', forecast_data=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
